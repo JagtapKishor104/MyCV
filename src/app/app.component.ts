b34ab68401225549942d936755002ee878bef7e9
kishor <jagtapkishor104@gmail.com>
2022-04-22 15:34:08 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {
+
+
+  }
+  ngOnInit(): void {
+
+    var name: any = document.getElementById('getname');
+    name.onclick = function () {
+      alert('You are Owsome');
+    }
+
+    var hello1: any = document.querySelector('#hello1');
+    hello1.addEventListener('click', () => {
+      alert("i am Working");
+      alert('are you Fine');
+    }
+
+    )
+
+    var paraname = () => {
+      alert('Paragraph Working');
+    }
+
+    var para3: any = document.querySelector(".para");
+    para3.addEventListener('click', paraname);
+
+  }
   title = 'Practise';
+
 }
